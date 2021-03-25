d0a405b77746f211f201eb5ad7c413d01b2cfda9
Ryo Fuj <ryo176999@gmail.com>
2021-03-24 23:38:37 -0700

    init OneMileRoom
    created OneMileRoom on xcode as ios app.

---
+++ OneMileRoom/OneMileRoom/OneMileRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneMileRoomApp.swift
+//  OneMileRoom
+//
+//  Created by ryo fujimura on 3/24/21.
+//
+
+import SwiftUI
+
+@main
+struct OneMileRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
